46a8f049f727a0fbf2362d3a9a247326a406c26b
Julien Leray <leray.jul@gmail.com>
2025-07-16 11:45:55 +0200

    test redeploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import { defineOrganization } from 'nuxt-schema-org/schema'
 
